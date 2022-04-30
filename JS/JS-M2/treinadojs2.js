2b82ee8adcdcfd4b7d32b5d2d9fd12390ced5c0d
Rodrigo L. Dominguez <rodrigoleiro@gmail.com>
2022-04-29 23:28:08 -0300

    att

---
+++ JS/JS-M2/treinadojs2.js
@@ -0,0 +1,18 @@
+let nota1 = ("digite nota1");
+let nota2 = ("digite nota1");
+let nota3 = ("digite nota1");
+let nota4 = ("digite nota1");
+
+let soma = Number(nota1 + nota2 + nota3 + nota4);
+
+let media = (soma / 4);
+
+
+if (media >= 8){
+    
+    console.log("aprovado")
+
+}else{
+    
+    console.log('recuperação')
+}
---
+++ JS/JS-M2/treinandojs.js
@@ -0,0 +1,21 @@
+   let nomes = ["Rodrigo", "Aline"]
+   let cor = "azul";
+    let cor2 = "verde";
+    
+    let meuCarro = "cinza";
+    
+   
+    
+if (meuCarro == "cinza"){
+    meuCarro = "rosa";
+}else {(meuCarro == "azul")
+    meuCarro = "verde";
+}
+
+console.log(meuCarro);
+
+
+
+
+
+
