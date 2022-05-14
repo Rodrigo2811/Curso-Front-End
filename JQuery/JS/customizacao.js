874f7f572d5088d4b1e2060a1d31897122e7f06d
Rodrigo L. Dominguez <rodrigoleiro@gmail.com>
2022-05-14 17:43:50 -0300

    criando pasta webpack

---
+++ JQuery/JS/customizacao.js
@@ -0,0 +1,12 @@
+// Instancia jQuery e evita conflitos.
+jQuery( function($){
+
+   
+    let titulos = $("h4") //seletor de tag
+    let classes = $(".tag") //seletor de class
+    let id = $("#id...") //seletor por id
+    
+})
+
+
+
