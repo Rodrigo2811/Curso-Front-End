dda6c81459de38ef26c929ce9d86d63950a227b7
Rodrigo L. Dominguez <rodrigoleiro@gmail.com>
2022-05-14 22:16:43 -0300

    exercicio lance

---
+++ WebPack/src/index.js
@@ -0,0 +1,10 @@
+
+function title(){
+
+    const elmeh1 = document.createElement('h1')
+    elemh1.innerHTML = 'Hello world'
+    return elemh1
+
+}
+
+document.body.appendChild(title())
---
+++ WebPack/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+
+Module.exports = {
+    entry: {
+        filename: 'main.js',
+        path: path.resolve(_dirname, 'dist')
+    }
+}
