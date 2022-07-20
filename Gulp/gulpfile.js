8f6d6a0b0ad84ed82b0ef4468a5d11867d86ecdc
Rodrigo <rodrigoleiro@gmail.com>
2022-07-19 22:51:15 -0300

    Modulo 2 Gulp

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -4,42 +4,53 @@ const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
+const htmlmin = require('gulp-htmlmin')
+const { parallel } = require('gulp')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 
 
 
-function tarefasCSS(cb){
+function tarefasCSS(callback){
 
-    return gulp.src('./assets/css/style.css', 
+     gulp.src('./assets/css/style.css', 
     './node_modules/bootstrap/dist/css/bootstrap.min.css',
      './node_modules/font-awesome/css/font-awesome.min.css',
          './vendor/owl/css/owl.css')
-
-        .pipe(concat('libs.css'))
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist/css'))
         
-
+        .pipe(concat('libs.css'))       //mescla arquivos
+        .pipe(cssmin())                 //minifica o css
+        .pipe(rename({suffix: '.min'})) //nomeia o arquivo 
+        .pipe(gulp.dest('./dist/css'))  //cria arquivo em novo diretorio
+        
+        return callback()
 }
 
-function tarefaJS(){
+function tarefaJS(callback){
 
-    return gulp.src('./node_modules/jquery/dist/jquery.min.js',
+     gulp.src('./node_modules/jquery/dist/jquery.min.js',
      './node_modules/bootstrap/dist/js/bootstrap.js',
      './vendor/owl/js/owl.js'
      )
-    .pipe(concat('libs.js'))
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./dist/JS'))
+     .pipe(babel({
+        comments:false,
+        presets: ['@babel/env']
+     }))
+    .pipe(concat('libs.js'))  //mescla os arquivos
+    .pipe(uglify())           //unifica o JavaScript  
+    .pipe(rename({suffix: '.min'})) //nomeia o arquivo
+    .pipe(gulp.dest('./dist/JS'))   //Cria arquivo em novo diretorio
 
+
+    return callback()
 }
 
 function tarefaImagem(){
 
     return gulp.src('./assets/images/*')
-    .pipe(image({
+    .pipe(image({                       //mescla imagens
     pngquant: true,
     optipng: false,
     zopflipng: true,
@@ -52,11 +63,33 @@ function tarefaImagem(){
         
     }))
 
-    .pipe(gulp.dest('./dist/image'))
+    .pipe(gulp.dest('./dist/image')) //cria arquivos em novo diretorio
 }
+
+function tarefasHTML(callback){
+    gulp.src('./assets/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist'))
+
+    return callback()
+
+}
+
+gulp.task('serve', function(){
+    
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./dist/**/*').on('change', reload)
+})
 
 exports.styles = tarefasCSS
 exports.scripts = tarefaJS
 exports.image = tarefaImagem
+
+exports.default = parallel (tarefasHTML, tarefasCSS, tarefaJS )
+
 
 
