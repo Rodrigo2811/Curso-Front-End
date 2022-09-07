1dfc2ea7fe520fc05493ef12954969365398c392
Rodrigo lopes <rodrigoleiro@gmail.com>
2022-09-06 21:04:21 -0300

    exercicio gulp-sass

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -7,76 +7,86 @@ const image = require('gulp-imagemin')
 const htmlmin = require('gulp-htmlmin')
 const { parallel } = require('gulp')
 const babel = require('gulp-babel')
+const sass = require('gulp-sass')(require('node-sass'))
 const browserSync = require('browser-sync').create()
 const reload = browserSync.reload
 
 
 
+function tarefasCSS(callback) {
 
-function tarefasCSS(callback){
+    gulp.src('./assets/css/style.css',
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './node_modules/font-awesome/css/font-awesome.min.css',
+        './vendor/owl/css/owl.css')
 
-     gulp.src('./assets/css/style.css', 
-    './node_modules/bootstrap/dist/css/bootstrap.min.css',
-     './node_modules/font-awesome/css/font-awesome.min.css',
-         './vendor/owl/css/owl.css')
-        
         .pipe(concat('libs.css'))       //mescla arquivos
         .pipe(cssmin())                 //minifica o css
-        .pipe(rename({suffix: '.min'})) //nomeia o arquivo 
+        .pipe(rename({ suffix: '.min' })) //nomeia o arquivo 
         .pipe(gulp.dest('./dist/css'))  //cria arquivo em novo diretorio
-        
-        return callback()
+
+    return callback()
 }
 
-function tarefaJS(callback){
+function tarefaSASS(callback) {
 
-     gulp.src('./node_modules/jquery/dist/jquery.min.js',
-     './node_modules/bootstrap/dist/js/bootstrap.js',
-     './vendor/owl/js/owl.js'
-     )
-     .pipe(babel({
-        comments:false,
-        presets: ['@babel/env']
-     }))
-    .pipe(concat('libs.js'))  //mescla os arquivos
-    .pipe(uglify())           //unifica o JavaScript  
-    .pipe(rename({suffix: '.min'})) //nomeia o arquivo
-    .pipe(gulp.dest('./dist/JS'))   //Cria arquivo em novo diretorio
+    gulp.src('./assets/scss/*.scss')
+        .pipe(sass()) // transforma aquivo sass em csss
+        .pipe(gulp.dest('./dist/css'))
+
+    return callback()
+
+}
+
+function tarefaJS(callback) {
+
+    gulp.src('./node_modules/jquery/dist/jquery.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './vendor/owl/js/owl.js'
+    )
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('libs.js'))  //mescla os arquivos
+        .pipe(uglify())           //unifica o JavaScript  
+        .pipe(rename({ suffix: '.min' })) //nomeia o arquivo
+        .pipe(gulp.dest('./dist/JS'))   //Cria arquivo em novo diretorio
 
 
     return callback()
 }
 
-function tarefaImagem(){
+function tarefaImagem() {
 
     return gulp.src('./assets/images/*')
-    .pipe(image({                       //mescla imagens
-    pngquant: true,
-    optipng: false,
-    zopflipng: true,
-    jpegRecompress: false,
-    mozjpeg: true,
-    gifsicle: true,
-    svgo: true,
-    concurrent: 10,
-    quiet: true
-        
-    }))
+        .pipe(image({                       //mescla imagens
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
 
-    .pipe(gulp.dest('./dist/image')) //cria arquivos em novo diretorio
+        }))
+
+        .pipe(gulp.dest('./dist/image')) //cria arquivos em novo diretorio
 }
 
-function tarefasHTML(callback){
+function tarefasHTML(callback) {
     gulp.src('./assets/**/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('./dist'))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
 
     return callback()
 
 }
 
-gulp.task('serve', function(){
-    
+gulp.task('serve', function () {
+
     browserSync.init({
         server: {
             baseDir: "./dist"
@@ -88,9 +98,10 @@ gulp.task('serve', function(){
 exports.styles = tarefasCSS
 exports.scripts = tarefaJS
 exports.image = tarefaImagem
+exports.sass = tarefaSASS
 
 
-exports.default = parallel (tarefasHTML, tarefasCSS, tarefaJS )
+exports.default = parallel(tarefasHTML, tarefasCSS, tarefaJS, tarefaSASS)
 
 
 
---
+++ JavaScrip_Avancado/src/moduloImports.js
@@ -0,0 +1,10 @@
+// import boostrap from 'bootstrap'
+// import * as nomeQuaquer from './modulesExport'
+import Cachorro from '..src/classes'
+
+const cachorro = new Cachorro('cachorro')
+cachorro.falar()
+cachorro.comer()
+cachorro.dormir()
+
+console.log(cachorro)
